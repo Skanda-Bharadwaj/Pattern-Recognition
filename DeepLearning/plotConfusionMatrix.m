dc5177bf67bbd6caccfd3452b5a31e200652de10
Skanda Bharadwaj <skandabharadwaj94@gmail.com>
2019-07-02 14:38:30 -0400

    Plotting Confusion Matrices

---
+++ DeepLearning/plotConfusionMatrix.m
@@ -0,0 +1,53 @@
+%% Plot Confusion Matrix
+%--------------------------------------------------------------------------
+%  
+% Plot the confusion matrix for predictions.
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Author : Skanda Bharadwaj 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%
+function plotConfusionMatrix(groundTruthLabels, predictedLabels, text)
+    numClasses = length(unique(groundTruthLabels));
+    targets = zeros(numClasses, size(predictedLabels, 1));
+    outputs = zeros(numClasses, size(predictedLabels, 1));
+    
+%fprintf('Creating 1-of-k targets...\n');
+    for ii = 1:numClasses
+        for jj = 1:size(groundTruthLabels, 1)
+            if groundTruthLabels(jj, 1) == 6
+                groundTruthLabels(jj, 1) = 2;
+            elseif groundTruthLabels(jj, 1) == 10
+                    groundTruthLabels(jj, 1) = 3;
+            end
+            if (groundTruthLabels(jj, 1) == ii)
+                targets(ii, jj) = 1;
+            else
+                targets(ii, jj) = 0;
+            end
+        end
+        
+    end
+    
+%fprintf('Creating 1-of-k outputs...\n');
+    for pp = 1:numClasses
+        for qq = 1:size(predictedLabels, 1)
+            if predictedLabels(qq, 1) == 6
+                predictedLabels(qq, 1) = 2;
+            elseif predictedLabels(qq, 1) == 10
+                predictedLabels(qq, 1) = 3;
+            end
+            if (predictedLabels(qq, 1) == pp)
+                outputs(pp, qq) = 1;
+            else
+                outputs(pp, qq) = 0;
+            end
+        end
+    end
+  
+%fprintf('Plotting Confusion Matrix...\n');
+    plotconfusion(targets, outputs, text);
+    set(findall(0,'FontName','Helvetica','FontSize',10),...
+    'FontName','Times New Roman','FontSize',16);
+end
